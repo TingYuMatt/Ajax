bf769524809a2f56e311389ab0eaee547cba70b1
cowbae123 <cowbae12345@gmail.com>
2023-07-17 08:56:33 +0800

    0717Homewoek

---
+++ Msit147Site/Controllers/ApiController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Msit147Site.Models;
+
+namespace Msit147Site.Controllers
+{
+    
+        
+    public class ApiController : Controller
+    {
+        private readonly DemoContext _context;
+        public ApiController(DemoContext context) 
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return Content("<h2>Hello!!</h2>","text/html");
+        }
+
+        public IActionResult Cities() 
+        {
+            var cities = _context.Address.Select(c => c.City).Distinct();
+
+            return Json(cities);
+        }
+
+        public IActionResult Districts(string city) {
+        var districts = _context.Address.Where(x=>x.City==city).Select(x=>x.SiteId).Distinct();
+            return Json(districts);
+        }
+
+        public IActionResult Roads(string districts)
+        {
+            var roads = _context.Address.Where(o => o.SiteId== districts).Select(x => x.Road).Distinct();
+            return Json(roads);
+        }
+
+        public IActionResult CheckAcount(string name) 
+        {
+            var confirm = _context.Members.FirstOrDefault(x => x.Name==name);
+            return Json(confirm);
+        }
+    }
+}
---
+++ Msit147Site/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Msit147Site.Models;
+using System.Diagnostics;
+
+namespace Msit147Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult First()
+        {
+            return View();
+        }
+
+        public IActionResult Homework1() 
+        {
+            return View();
+        }
+        public IActionResult Homework2()
+        {
+            return View();
+        }
+        public IActionResult Homework3()
+        {
+            return View();
+        }
+        public IActionResult Homework4()
+        {
+            return View();
+        }
+        public IActionResult Address()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
